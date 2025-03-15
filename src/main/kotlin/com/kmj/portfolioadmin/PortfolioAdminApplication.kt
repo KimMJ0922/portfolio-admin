eaf1b60afa8cf48cea9a3c6d1ecbff0bf4e991ad
kmj <zkxm_0@naver.com>
2025-03-15 22:07:55 +0900

    프로젝트 생성

---
+++ src/main/kotlin/com/kmj/portfolioadmin/PortfolioAdminApplication.kt
@@ -0,0 +1,11 @@
+package com.kmj.portfolioadmin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PortfolioAdminApplication
+
+fun main(args: Array<String>) {
+    runApplication<PortfolioAdminApplication>(*args)
+}
---
+++ src/test/kotlin/com/kmj/portfolioadmin/PortfolioAdminApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kmj.portfolioadmin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PortfolioAdminApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
